a7c5094943f7888943bf2fbb08240103e4730d5c
pengxuanHuang <pacers0406@gmail.com>
2025-05-22 14:46:17 +0200

    add docstrings to the custom pylint functions

--- pylint_nan_check/__init__.py
+++ pylint_nan_check/__init__.py
@@ -1,4 +1,10 @@
+'''
+Register the NaNComparisonChecker with pylint
+'''
 from .nan_comparison_checker import NaNComparisonChecker
 
 def register(linter):
+    '''
+    Register the NaNComparisonChecker with pylint
+    '''
     linter.register_checker(NaNComparisonChecker(linter))
--- pylint_nan_check/nan_comparison_checker.py
+++ pylint_nan_check/nan_comparison_checker.py
@@ -1,8 +1,13 @@
+'''
+Custom Pylint checker to check for NaN Equivalence Comparison Misused.
+'''
 from pylint.checkers import BaseChecker
 import astroid
 
 class NaNComparisonChecker(BaseChecker):
-
+    '''
+    Custom NaNComparisonChecker to check for NaN equivalence comparison misuse.
+    '''
     name = "nan-comparison-checker"
     msgs = {
         "W5501": (
@@ -13,6 +18,10 @@ class NaNComparisonChecker(BaseChecker):
     }
 
     def visit_compare(self, node):
+        '''
+        Checks for comparisons using '== np.nan' or '!= np.nan' and reports a warning,
+        as these comparisons always return False. Suggests using np.isnan() instead.
+        '''
         for side in (node.left, node.ops[0][1]):
             if isinstance(side, astroid.Attribute) \
                     and side.attrname == "nan" \
